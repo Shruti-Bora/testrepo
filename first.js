1e53e247a4c72910c333cbe0e5e280dcd43032b4
Shruti-Bora <shrutibora55@gmail.com>
2024-12-16 17:36:37 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,9 @@
+const Student = {
+    fullName : "Shruti bora",
+    age : 24,
+    cgpa : 8.9,
+    isPass : true,
+};
+Student["age"]=Student["age"]+1;
+Student["fullName"]="Tanish Bora";
+console.log(Student);
